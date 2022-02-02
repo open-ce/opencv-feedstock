9ba772e37052ce56a0e8d8f632e2e2c0cd61cde3
Nishidha Panpaliya <npanpa23@in.ibm.com>
2022-02-02 06:43:35 -0600

    updated recipe to add P10 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,7 +28,24 @@ export CXXFLAGS="$CXXFLAGS -D__STDC_CONSTANT_MACROS"
 export CPPFLAGS="${CPPFLAGS//-std=c++17/-std=c++11}"
 export CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++11}"
 
-export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib -lopenjp2"
+export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+
+if [[ $ppc_arch == "p10" ]]
+then
+    if [[ -z "${GCC_10_HOME}" ]];
+    then
+        echo "Please set GCC_10_HOME to the install path of gcc-toolset-10"
+        exit 1
+    else
+        AR=${GCC_10_HOME}/bin/ar
+        LD=${GCC_10_HOME}/bin/ld
+        NM=${GCC_10_HOME}/bin/nm
+        OBJCOPY=${GCC_10_HOME}/bin/objcopy
+        OBJDUMP=${GCC_10_HOME}/bin/objdump
+        RANLIB=${GCC_10_HOME}/bin/ranlib
+        STRIP=${GCC_10_HOME}/bin/strip
+    fi
+fi
 
 CMAKE_TOOLCHAIN_CMD_FLAGS=""
 CMAKE_TOOLCHAIN_CMD_FLAGS="${CMAKE_TOOLCHAIN_CMD_FLAGS} -DCMAKE_AR=${AR}"
